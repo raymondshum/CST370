412b90ee5c42c84957bc807cfc54c8d2b1dc5379
Raymond Shum <rshum@csumb.edu>
2021-10-27 21:02:04 -0700

    completed hw2

---
+++ hw1/hw1_2/hw1_2.java
@@ -0,0 +1,50 @@
+/**
+ * Title: hw1_2.cpp
+ * Abstract: This program reads input numbers from a user and displays the numbers
+ *           that appear with the highest frequency, in descending order.
+ * Author: Raymond Shum
+ * ID: 9030
+ * Date: 11/2/2021
+ */
+
+import java.util.*;
+
+public class hw1_2 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        StringBuilder result = new StringBuilder("Number:");
+        int maxFreq = 0;
+        int numElements = input.nextInt();
+
+        // TreeMap is in reverse order for descending order result
+        Map<Integer, Integer> frequencyList = new TreeMap<>(Collections.reverseOrder());
+
+        // Count frequency of all incoming numbers
+        for (int i = 0; i < numElements; i++) {
+            int number = input.nextInt();
+
+            // Set number as 1 in Treemap if does not exist
+            if (!frequencyList.containsKey(number)) {
+                frequencyList.put(number, 1);
+            } else { // Add one to frequency count if number exists
+                int newFreq = frequencyList.get(number) + 1;
+                maxFreq = Math.max(newFreq, maxFreq); //tracks highest frequency
+                frequencyList.put(number, newFreq);
+            }
+        }
+
+        System.out.printf("Frequency:%d\n", maxFreq);
+
+        // Prints key if value matches highest frequency number (desc order)
+        for (Integer key : frequencyList.keySet()) {
+            if(frequencyList.get(key) == maxFreq) {
+                result.append(key + " ");
+            }
+        }
+
+        System.out.println(result);
+
+        input.close();
+    }
+}
