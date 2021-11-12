ed281ee2ba18858f7007a045f5b595193c1fd450
Raymond Shum <rshum@csumb.edu>
2021-11-11 21:57:38 -0800

    Updated Comments

--- hw3/hw3_2/hw3_2.java
+++ hw3/hw3_2/hw3_2.java
@@ -1,7 +1,9 @@
 
 /**
  * TITLE: hw3_2.java 
- * ABSTRACT: 
+ * ABSTRACT: This program reads input graph data from a user. Then it
+ * presents a solution to the travelling salesman problem (TSP) in the
+ * form of a path and cost.
  * AUTHOR: Raymond Shum 
  * ID: 9030 
  * DATE: 11/16/2021
@@ -60,6 +62,10 @@ public class hw3_2 {
         input.close();
     }
 
+    /**
+     * Prints 2D matrix (for debug purposes)
+     * @param matrix int[][] to be printed
+     */
     static void printMatrix(int[][] matrix) {
         String test = "";
         for (int[] array : matrix) {
@@ -71,8 +77,15 @@ public class hw3_2 {
         }
     }
 
+    /**
+     * Find possible permutations of paths (except for starting Vertex). Directly modifies the
+     * permutations ArrayList. 
+     * @param numVert int number of vertices in the graph
+     * @param startingVertex int vertex to be excluded from permutations
+     * @param permutations ArrayList<int[]> that will hold all possible permutations
+     */
     static void findPerm(int numVert, int startingVertex, ArrayList<int[]> permutations) {
-        ArrayList<Integer> vertList = new ArrayList<Integer>(numVert - 1);
+        ArrayList<Integer> vertList = new ArrayList<Integer>();
 
         // initialize array of possible vertices
         for (int i = 0; i < numVert; i++) {
@@ -81,25 +94,36 @@ public class hw3_2 {
             }
         }
 
-        permute(vertList.stream().mapToInt(i -> i).toArray(), 0, permutations, startingVertex);
+        // add all possible path permutations to permutations ArrayList 
+        permute(vertList.stream().mapToInt(i -> i).toArray(), 0, permutations);
     }
 
-    static void permute(int[] input, int startindex, ArrayList<int[]> permutations, int startingVertex) {
+    /**
+     * This function modifies Dr. Byun's example permutation program from Replit. Instead of printing,
+     * it adds each new int[] to ArrayList<int[]> permutations.
+     * 
+     * Source: https://replit.com/@YBYUN/JavaPermutations 
+     * @param input int[] array containing vertices in graph, excluding starting vertex
+     * @param startindex int position of starting index on input
+     * @param permutations ArrayList<int[]> that permutations are pushed onto
+     */
+    static void permute(int[] input, int startindex, ArrayList<int[]> permutations) {
         int size = input.length;
 
+        // push unique permutation onto permutations ArrayList
         if (size == startindex + 1) {
             int[] temp = new int[size];
             for (int i = 0; i < size; i++) {
                 temp[i] = input[i];
             }
             permutations.add(temp);
-        } else {
+        } else { //recursively generate permutations for each vertex
             for (int i = startindex; i < size; i++) {
                 int temp = input[i];
                 input[i] = input[startindex];
                 input[startindex] = temp;
 
-                permute(input, startindex + 1, permutations, startingVertex);
+                permute(input, startindex + 1, permutations);
 
                 temp = input[i];
                 input[i] = input[startindex];
@@ -107,14 +131,24 @@ public class hw3_2 {
             }
         }
     }
-
+    
+    /**
+     * Calculates lowest cost path & determines index within permutations ArrayList. Returns an int[] of
+     * int[0] = index and int[1] = cost. If no path exists, it returns { -1, -1 }.
+     * @param graph int[][] containing values of costs between vertices
+     * @param permutations ArrayList<int[]> containing all possible path permutations excluding starting vertex
+     * @param startingVertex int representing starting and ending vertex
+     * @return int[] array with [0] = index and [1]= cost
+     */
     static int[] calculateCosts(int[][] graph, ArrayList<int[]> permutations, int startingVertex) {
 
         int index = -1;
         int min = Integer.MAX_VALUE;
 
+        // Calculate cost for each path in permutations
         for (int i = 0; i < permutations.size(); i++) {
 
+            // create temp array with starting vertex added to front and end
             ArrayList<Integer> temp = new ArrayList<Integer>();
             temp.add(0, startingVertex);
             for (int element : permutations.get(i)) {
@@ -125,26 +159,36 @@ public class hw3_2 {
             boolean noPath = false;
             int cost = 0;
 
+            // Calculate cost of path if it exists
             for (int j = 0; j < temp.size() - 1; j++) {
                 noPath = false;
                 int nextVert = j + 1;
 
+                // Exit loop if next vertex cannot be reached
                 if (graph[temp.get(j)][temp.get(nextVert)] == 0) {
                     noPath = true;
                     break;
-                } else {
+                } else { // add cost of traveling to next vertex
                     cost += graph[temp.get(j)][temp.get(nextVert)];
                 }
             }
 
+            // update index & min cost values if path exists
             if (!noPath) {
+                // update index if the current cost is the new minimum
                 index = Math.min(min, cost) == cost ? i : index;
                 min = Math.min(min, cost);
             }
         }
+        // return {-1, -1} if no paths exist
         return new int[] { index, index == -1 ? -1 : min };
     }
 
+    /**
+     * Calculate the factorial of num
+     * @param num
+     * @return int representing num!
+     */
     static int factorial(int num) {
         if (num == 1) {
             return num;
