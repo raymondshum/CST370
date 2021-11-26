0f08d99a5c9ec1f83c85e3c80b4da10d75aa73f5
Raymond Shum <rshum@csumb.edu>
2021-11-25 19:42:31 -0800

    hw4_3

--- hw4/hw4_3/hw4_3.java
+++ hw4/hw4_3/hw4_3.java
@@ -1,3 +1,11 @@
+/**
+ * Title: hw4_3.java 
+ * Abstract: This program conducts topological sorting based on
+ * Kahn's algorithm (covered in the lecture).
+ * ID: 9030 
+ * Date: 11/30/2021
+ */
+
 import java.util.*;
 
 public class hw4_3 {
@@ -10,47 +18,66 @@ public class hw4_3 {
     }
 }
 
+/**
+ * This class takes user input and arranges it into
+ * an adjacency list.
+ */
 class Graph {
     int numEdge;
     int numVert;
     ArrayList<ArrayList<Integer>> adjList;
-
+    /**
+     * Default constructor. Takes preformatted user input.
+     */
     Graph() {
         Scanner myScanner = new Scanner(System.in);
         numVert = myScanner.nextInt();
         numEdge = myScanner.nextInt();
         adjList = new ArrayList<>(numVert);
 
+        // Initialize adjacency list
         for (int i = 0; i < numVert; i++) {
             adjList.add(new ArrayList<Integer>());
         }
 
+        // Populate adjacency list with user input
         for (int i = 0; i < numEdge; i++) {
             int thisVert = myScanner.nextInt();
             int edgeTo = myScanner.nextInt();
             adjList.get(thisVert).add(edgeTo);
         }
 
+        // Sort edges in ascending order
         for (int i = 0; i < numVert; i++) {
             Collections.sort(adjList.get(i));
         }
-
         myScanner.close();
     }
 
+    /**
+     * Getter. Number of edges in graph.
+     */
     public int getNumEdge() {
         return numEdge;
     }
 
+    /**
+     * Getter. Number of vertices in graph.
+     */
     public int getNumVert() {
         return numVert;
     }
 
-    // Shallow copy
+    /**
+     * Getter. Returns shallow copy of adjacency list.
+     */
     public ArrayList<ArrayList<Integer>> getAdjList() {
         return adjList;
     }
 
+    /**
+     * Displays adjacency list for debug purposes.
+     */
     public String toString() {
         StringBuilder result = new StringBuilder();
         int count = 0;
@@ -66,10 +93,17 @@ class Graph {
     }
 }
 
+/**
+ * Holds methods for topological sorting based on Kahn's algorithm.
+ */
 class Kahn {
     int[] inDegree;
     Graph myGraph;
 
+    /**
+     * Default constructor.
+     * @param myGraph Graph of user input
+     */
     Kahn(Graph myGraph) {
         this.myGraph = myGraph;
         int numVert = myGraph.getNumVert();
@@ -78,8 +112,14 @@ class Kahn {
         Arrays.fill(inDegree, 0);
     }
 
+    /**
+     * Updates initialized inDegree[] to represent Step 1 values.
+     */
     public void calcInDegree() {
         ArrayList<ArrayList<Integer>> adjList = myGraph.getAdjList();
+
+        // inDegree[vertex] starts at 0 for all values. Adds 1 to each vertex
+        // (inDegree[vertex]) for each incoming edge.
         for (int i = 0; i < adjList.size(); i++) {
             for (int j = 0; j < adjList.get(i).size(); j++) {
                 inDegree[adjList.get(i).get(j)] += 1;
@@ -87,67 +127,61 @@ class Kahn {
         }
     }
 
+    /**
+     * Prints formatted values of inDegree[] to console.
+     */
     public void printInDegree() {
         for (int i = 0; i < inDegree.length; i++) {
             System.out.printf("In-degree[%d]:%d\n", i, inDegree[i]);
         }
     }
 
+    /**
+     * Calculates and prints order of visited nodes when performing
+     * topological sorting using Kahn's algorithm.
+     */
     public void calcOrder() {
         ArrayList<ArrayList<Integer>> adjList = myGraph.getAdjList();
-        ArrayList<Integer> lastAdded = new ArrayList<>();
+        LinkedList<Integer> queue = new LinkedList<>(); 
+        LinkedList<String> result = new LinkedList<>(); // Holds sorted order.
 
-        boolean hasCycle = false;
-        boolean visited[] = new boolean[myGraph.getNumVert()];
-        Arrays.fill(visited, false);
-
-        int numVisited = 0;
-        Queue<Integer> queue = new LinkedList<>();
-        StringBuilder result = new StringBuilder();
-
+        // Add initial source nodes (vertices) to the queue.
         for (int i = 0; i < inDegree.length; i++) {
-            if (inDegree[i] == 0 && visited[i] == false) {
+            if (inDegree[i] == 0) {
                 queue.offer(i);
-                visited[i] = true;
-                numVisited++;
-                lastAdded.add(i);
-                result.append(String.format("%d->", i));
             }
         }
 
-        while (numVisited < myGraph.getNumVert() && !hasCycle) {
+        // This will traverse all vertices that are not part of a cycle. Once the queue is empty,
+        // there are no longer any source nodes found in the graph.
+        while (queue.size() > 0) {
+            int vertex = queue.poll();
 
-            while (queue.size() > 0) {
-                int vertex = queue.poll();
-                for (int edge : adjList.get(vertex)) {
-                    inDegree[edge] -= 1;
-                    if (inDegree[edge] == 0) {
-                        queue.offer(edge);
-                        visited[edge] = true;
-                        numVisited++;
-                        lastAdded.add(edge);
-                        result.append(String.format("%d->", edge));
-                    }
+            // Add dequeued node to result, formatting is different if it is the first node.
+            result.add(result.size() == 0 ? String.format("Order:%d", vertex) : String.format("->%d", vertex));
+
+            // After dequeueing node, decrement inDegree values for all vertices with incoming edges
+            // from that node.
+            for (int edge : adjList.get(vertex)) {
+                inDegree[edge] -= 1;
+
+                // If inDegree becomes 0, add the vertex to the queue as a new source node
+                if (inDegree[edge] == 0) {
+                    queue.offer(edge);
                 }
             }
-
-            int numSources = 0;
-            for (int i = 0; i < inDegree.length; i++) {
-                if ((inDegree[i] == 0 && visited[i] == false) || (inDegree[i] == 0 && lastAdded.contains(i))) {
-                    numSources++;
-                }
-            }
-            if (numSources == 0) {
-                hasCycle = true;
-            }
-            lastAdded.clear();
         }
 
-        if (hasCycle) {
+        // Nodes in result & numVert will differ if there is a cycle in the graph 
+        // (no new source nodes)
+        if (result.size() != myGraph.getNumVert()) {
             System.out.println("No Order:");
         } else {
-            result = result.delete(result.length() - 2, result.length());
-            System.out.println("Order:" + result.toString());
+            for (String str : result) {
+                System.out.printf(str);
+            }
+            System.out.println();
         }
     }
+
 }
