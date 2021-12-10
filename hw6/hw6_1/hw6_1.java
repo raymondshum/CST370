3aab76a5c3fef482839bc294a978797bd6ece700
Raymond Shum <rshum@csumb.edu>
2021-12-09 20:32:34 -0800

    Updated comments

--- hw6/hw6_1/hw6_1.java
+++ hw6/hw6_1/hw6_1.java
@@ -1,11 +1,30 @@
+
+/**
+ * Title: hw6_1.java 
+ * Abstract: This program takes user input and builds a 2D
+ * matrix, representing a board with coins on cells marked
+ * with "1" (and nothing on cells marked with 0). Starting
+ * from the upper left hand corner and traversing to the
+ * lower right, it calculates the optimal path in which a
+ * maximum number of coins can be acquired. Traversal can
+ * only occur from the current cell to the cell on the 
+ * right or below.
+ * Author: Raymond Shum 
+ * ID: 9030 
+ * Date: 12/14/2021
+ */
+
 import java.util.*;
 
 public class hw6_1 {
 
-    int[][] board;
-    int[][] reference;
-    int coins;
+    int[][] board; // Stores dynamic programming results
+    int[][] reference; // Stores original user input
+    int coins; // Used for max coins
 
+    /**
+     * Default constructor
+     */
     hw6_1() {
         Scanner myScanner = new Scanner(System.in);
 
@@ -14,9 +33,11 @@ public class hw6_1 {
 
         coins = 0;
 
+        // The board is intended to start at [1,1] for both arrays
         board = new int[row + 1][col + 1];
         reference = new int[row + 1][col + 1];
 
+        // Initialize both 2d arrays
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 board[i + 1][j + 1] = myScanner.nextInt();
@@ -25,10 +46,15 @@ public class hw6_1 {
         }
 
         buildResultArray();
-
         myScanner.close();
     }
 
+    /**
+     * Helper for constructor. Value of current cell
+     * is equal to the greater of the cell above or
+     * cell to the left plus the contents of the
+     * current cell.
+     */
     private void buildResultArray() {
         for (int i = 1; i < board.length; i++) {
             for (int j = 1; j < board[i].length; j++) {
@@ -39,19 +65,33 @@ public class hw6_1 {
         }
     }
 
+    /**
+     * Returns optimal path and max coins by tracing
+     * path from the lower right hand corner, back to
+     * the upper left hand corner.
+     * 
+     * @return String Containing formatted output.
+     */
     public String backTrace() {
+        // Results are popped from a stack (reverse order)
         Deque<String> stack = new ArrayDeque<>();
         int row = board.length - 1;
         int col = board[row].length - 1;
 
+        // Traverses from bottom right.
+        // Tie breaker occurs by picking left, so sometimes
+        // the current position will be below upper left corner.
         while (row != 0 && col != 0) {
             String currentPos = String.format("(%d,%d)", row, col);
             stack.push(currentPos);
 
+            // Coins are picked up along the reverse path
             coins += reference[row][col];
 
             int leftCell = board[row][col - 1];
             int topCell = board[row - 1][col];
+
+            // Will always traverse left in case of a tie
             if (topCell > leftCell) {
                 row--;
             } else if (leftCell >= topCell) {
@@ -59,7 +99,9 @@ public class hw6_1 {
             }
         }
 
-        col++;
+        col++; // Set column back to 1 (0 due to while loop)
+
+        // Returns to upper left if current position has not reached it
         while (row != 0) {
             row--;
             if (row != 0) {
@@ -69,6 +111,7 @@ public class hw6_1 {
             }
         }
 
+        // Format results, retrieve forward path by popping stack
         String result = "Path:";
         result += stack.pop();
         int size = stack.size();
@@ -77,6 +120,7 @@ public class hw6_1 {
             result += stack.pop();
         }
 
+        // Format required output
         String result2 = String.format("Max coins:%d\n", coins);
         result2 += result;
 
@@ -85,6 +129,6 @@ public class hw6_1 {
 
     public static void main(String args[]) {
         hw6_1 test = new hw6_1();
-        System.out.println(test.backTrace());;
+        System.out.println(test.backTrace());
     }
 }
